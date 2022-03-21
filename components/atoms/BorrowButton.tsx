3e889c2ea0be0309abf50f80f673263e33dc4bdb
Luis <luisbohorquezlugo@hotmail.com>
2022-03-21 14:10:50 -0700

    fix the design of the deposit and borrow button

--- components/atoms/BorrowButton.tsx
+++ components/atoms/BorrowButton.tsx
@@ -1,8 +1,20 @@
 import React from "react";
 import { useNavigation } from '@react-navigation/native';
 import {Button} from "./Button";
+import { StyleSheet } from "react-native";
 
 export const BorrowButton = () => {
     const navigation = useNavigation();
-    return <Button title={"Borrow"} onPress={() => navigation.navigate("Borrow")} />
+    return <Button title={"Borrow"} onPress={() => navigation.navigate("Borrow")} style={styles.button} />
 };
+
+const styles = StyleSheet.create({
+    button: {
+        borderTopLeftRadius: 0,
+        borderBottomLeftRadius: 0,
+        borderBottomRightRadius: 20,
+        borderTopRightRadius: 20,
+        padding: 10,
+        backgroundColor: "rgba(48, 200, 224, 0.8)",
+    }
+})
--- components/atoms/DepositButton.tsx
+++ components/atoms/DepositButton.tsx
@@ -1,8 +1,19 @@
 import React from "react";
 import { useNavigation } from '@react-navigation/native';
 import { Button } from "./Button";
+import { StyleSheet } from "react-native";
 
 export const DepositButton = () => {
     const navigation = useNavigation();
-    return <Button title={"Deposit"} onPress={() => navigation.navigate("Deposit")} />
+    return <Button title={"Deposit"} onPress={() => navigation.navigate("Deposit")} style={styles.button} />
 };
+
+const styles = StyleSheet.create({
+    button: {
+        borderTopLeftRadius: 20,
+        borderBottomLeftRadius: 20,
+        borderBottomRightRadius: 0,
+        borderTopRightRadius: 0,
+        padding: 10,
+    }
+})
