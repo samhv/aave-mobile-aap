ee0032ccbd9c9362fdca80c7e4527df8deabef1f
LuisB0605 <94773556+LuisB0605@users.noreply.github.com>
2022-03-24 16:54:00 -0700

    Revert "fix the design of the deposit and borrow button"

--- components/atoms/BorrowButton.tsx
+++ components/atoms/BorrowButton.tsx
@@ -1,17 +1,8 @@
 import React from "react";
 import { useNavigation } from '@react-navigation/native';
 import {Button} from "./Button";
-import { StyleSheet } from "react-native";
 
 export const BorrowButton = () => {
     const navigation = useNavigation();
-    return <Button title={"Borrow"} onPress={() => navigation.navigate("Borrow")} style={styles.button} />
+    return <Button title={"Borrow"} onPress={() => navigation.navigate("Borrow")} />
 };
-
-const styles = StyleSheet.create({
-    button: {
-        borderTopLeftRadius: 0,
-        borderBottomLeftRadius: 0,
-        backgroundColor: "rgba(48, 200, 224, 0.8)",
-    }
-})
--- components/atoms/DepositButton.tsx
+++ components/atoms/DepositButton.tsx
@@ -1,16 +1,8 @@
 import React from "react";
 import { useNavigation } from '@react-navigation/native';
 import { Button } from "./Button";
-import { StyleSheet } from "react-native";
 
 export const DepositButton = () => {
     const navigation = useNavigation();
-    return <Button title={"Deposit"} onPress={() => navigation.navigate("Deposit")} style={styles.button} />
+    return <Button title={"Deposit"} onPress={() => navigation.navigate("Deposit")} />
 };
-
-const styles = StyleSheet.create({
-    button: {
-        borderBottomRightRadius: 0,
-        borderTopRightRadius: 0,
-    }
-})
