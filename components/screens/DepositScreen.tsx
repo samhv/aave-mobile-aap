15393c532d5af577f4d4e1a72b5b5e2401306d0f
Luis <luisbohorquezlugo@hotmail.com>
2022-03-24 19:28:52 -0700

    working in the info of the header in the modal deposit

--- components/screens/DepositScreen.tsx
+++ components/screens/DepositScreen.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
-import {HeaderText} from "../typography";
+import {DepositText, HeaderText, StandardText} from "../typography";
 import {View} from "../atoms/View";
 import {Deposit} from "../organisms/Deposit";
 import {STYLES} from "../../constants";
+import {TokenIcon} from "../atoms/TokenIcon";
+import {aaveAddress} from "../../constants/tokens";
+import { SecondaryText } from '../typography';
+import { StyleSheet } from 'react-native';
 
 export const DepositScreen = () => {
   return (
@@ -20,8 +24,49 @@ export const TemplateScreen = ({ title, body }) => {
                 <HeaderText style={{ textAlign: "center" }}>
                     {title}
                 </HeaderText>
+                <View style={styles.container}>
+                    <View style={styles.backgroundV2}>
+                        <StandardText>
+                           V2 Markets
+                        </StandardText>
+                    </View>
+                    <View width={200}/>
+                    <View style={styles.depositA}>
+                        <View style={styles.containerDeposit}>
+                            <DepositText>Deposit APY</DepositText>
+                        </View>
+                        <View style={styles.containerAPY}>
+                            <TokenIcon address={aaveAddress} size={13}/>
+                            <SecondaryText style={{ marginLeft: 5 }}>
+                                Reward Aave
+                            </SecondaryText>
+                        </View>
+                    </View>
+                </View>
             </View>
             { body }
         </View>
     );
 };
+
+const styles = StyleSheet.create({
+    container: {
+        flexDirection: "row",
+        paddingTop: 20,
+        alignItems: "center",
+    },
+    containerAPY: {
+        flexDirection: "row",
+    },
+    backgroundV2: {
+        backgroundColor: STYLES.backgrounds.secondaryBackground,
+        padding: 7,
+        borderRadius: 8,
+    },
+    containerDeposit: {
+        padding: 8,
+    },
+    depositA: {
+        alignItems: "flex-end",
+    }
+})
