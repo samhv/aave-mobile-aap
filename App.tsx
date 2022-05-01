963da469ba8e795b897283ed79ea35d93add54e0
Oscar Santiago <oscar.santiago@outlook.com>
2022-04-30 17:31:55 -0700

    wip installing wallet connect

--- App.tsx
+++ App.tsx
@@ -1,13 +1,30 @@
 import React from 'react';
+// import { WalletConnectProvider } from "@walletconnect/react-native-dapp/dist/providers";
+// import AsyncStorage from "@react-native-async-storage/async-storage";
 import { StatusBar } from 'expo-status-bar';
 import {Navigation} from "./navigation";
 import {ReduxProvider, store} from "./redux-store/store";
 
+// import "react-native-get-random-values"
+// import "@ethersproject/shims"
+
 export default function App() {
       return (
+        // <WalletConnectProvider 
+        // clientMeta={{
+        //   description: 'Connect with WalletConnect',
+        //   url: 'https://aave.com/',
+        //   icons: ['https://cryptologos.cc/logos/aave-aave-logo.png?v=022'],
+        //   name: 'WalletConnect',
+        // }}
+        // redirectUrl={'expo://'}
+        // storageOptions= {{
+        //   asyncStorage: AsyncStorage,
+        // }}>
           <ReduxProvider>
             <Navigation/>
             <StatusBar style="auto" />
           </ReduxProvider>
+        // </WalletConnectProvider>
       );
 }
